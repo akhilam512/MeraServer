5d24ac57cc57d99a748dbbcef5a1b62685c72aa7
akhilam512 <helloimakhil@gmail.com>
2019-03-17 13:18:09 +0530

    Add server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+app.get('/home', function (req, res) {
+   res.sendFile( __dirname + "/public/" + "home.html" );
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
